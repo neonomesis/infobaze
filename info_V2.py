3705fb577294d04c28855e3dc18b8d6ea7d3e5ac
Bunescu Vasile <bunescu.vasile@gmail.com>
2021-08-28 23:31:07 +0300

    working

--- info_V2.py
+++ info_V2.py
@@ -11,23 +11,23 @@ from bs4 import BeautifulSoup
 import requests
 
 
-
-idno = ['1002600056626','1003602004112','1002600056626']
+idno = ["1002600056626", "1003602004112", "1002600056626"]
 
 # def idn():
 #     for x in idno:
-    
-#huineaua asta nu merge cu loop
+
+# huineaua asta nu merge cu loop
 soup = BeautifulSoup("html.parser", "lxml")
+url = "https://www.infobase.md/ro/search?page=1&q=/"
+
+
 def acces():
-    url = "https://www.infobase.md/ro/search?page=1&q="
-    for x in range(len(idno)):
-        pint = url + idno[x]
-        
-    else:
-        print('finish')
-        print(x)
+
+    for x in idno:
+        pint = url + x
+        print(pint)
     return pint
+
 
 driver = webdriver.Firefox(executable_path="../geckodriver")
 # driver.get(acces())
@@ -35,12 +35,14 @@ driver = webdriver.Firefox(executable_path="../geckodriver")
 
 driver.implicitly_wait(5)
 
+
 def enter_dno():
     driver.implicitly_wait(10)
     search_engine = driver.find_element(
         By.XPATH, '//*[@id="index"]/div/main/div/div[3]/div[1]/div/div/div/h2/a'
     ).click()
-   
+
+
 def parsing():
     pass
 
@@ -49,7 +51,8 @@ if __name__ == "__main__":
     try:
         for x in idno:
             # acces()
-            driver.get(acces())
+            driver.get(url + x)
+
         enter_dno()
     # except Exception as e:
     #     print(e)
