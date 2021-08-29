2a578101f4db85135784bfbdb31aefeb12bea084
Bunescu Vasile <bunescu.vasile@gmail.com>
2021-08-29 15:59:03 +0300

    saved change

--- info_V2.py
+++ info_V2.py
@@ -15,20 +15,14 @@ idno = ["1002600056626", "1003602004112", "1009600017846", "1013606000397"]
 
 soup = BeautifulSoup("html.parser", "lxml")
 url = "https://www.infobase.md/ro/search?page=1&q="
+driver = webdriver.Firefox(executable_path="../geckodriver")
 
 
 def acces():
-
     for x in idno:
         pint = url + x
         print(pint)
     return pint
-
-
-driver = webdriver.Firefox(executable_path="../geckodriver")
-
-driver.implicitly_wait(5)
-
 
 def enter_dno():
     driver.implicitly_wait(10)
@@ -37,22 +31,46 @@ def enter_dno():
     ).click()
 
 
-def parsing():
-    for y in parsing():
-        request = requests.get(url)
-        print(request.status_code)
-
-
-if __name__ == "__main__":
+#working to acces
+def acces():
     try:
         for x in idno:
             # acces()
             driver.get(url + x)
             enter_dno()
-            parsing()
+    finally:
+        pass
 
-    # except Exception as e:
-    #     print(e)
+def load_initial_website(url):
+    driver.get(url)
+#--------------------------------------------------------
+
+
+
+def parsing():
+    
+    soup = BeautifulSoup('html_code', 'html.parser')
+    containers = soup.find_all(class_='MuiTableCell-root MuiTableCell-body')
+
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    try:
+        acces()
+        # for x in idno:
+        #     # acces()
+        #     driver.get(url + x)
+        #     enter_dno()
+        parsing()
+
+    except Exception as e:
+        print(e)
     finally:
         driver.implicitly_wait(20)
         # driver.quit()
